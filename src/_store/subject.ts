e263f4c4a2a197989d5b878c4ae8dd4aefd91cc8
yonghuikim <kyhui1115@gmail.com>
2024-10-29 11:28:52 +0900

    feat: zustand store 추가

--- src/_store/subject.ts
+++ src/_store/subject.ts
@@ -2,12 +2,17 @@ import { create } from "zustand";
 
 interface subject {
   id: number;
+  year: string;
   setId: (num: number) => void;
+  setYear: (num: string) => void;
 }
 
 const useSubjectStore = create<subject>((set) => ({
   id: 0,
+  year: "2024",
+
   setId: (num) => set({ id: num }),
+  setYear: (year) => set({ year: year }),
 }));
 
 export default useSubjectStore;
