14661130b1469e388bfb4eae4e06e4db258f2c59
yonghuikim <kyhui1115@gmail.com>
2024-10-30 16:38:35 +0900

    design: css 일부 수정

--- src/app/survey/_components/_reportList/reportItem.tsx
+++ src/app/survey/_components/_reportList/reportItem.tsx
@@ -15,7 +15,7 @@ export default function ReportItem({ no, title, tables }: props) {
   const [tableHeight, setTableHeight] = useState(0);
 
   return (
-    <div className="relative flex border border-border-gray mt-6 p-2 rounded-md bg-white hover:shadow-container duration-500">
+    <div className="relative flex border border-border-gray mt-4 p-2 rounded-md bg-white hover:shadow-container duration-500">
       <span className="absolute font-semibold top-4 left-4">
         {no}. {title}
       </span>
