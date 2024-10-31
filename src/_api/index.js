dad8971b7b77bef975e50d1e628e6e34c1d35608
yonghuikim <kyhui1115@gmail.com>
2024-10-31 15:42:32 +0900

    chore: API URL 수정

--- src/_api/index.js
+++ src/_api/index.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
-  // baseURL: "https://tidy-ripple-plantain.glitch.me",
+  baseURL: "https://tidy-ripple-plantain.glitch.me",
 });
 
 export default api;
