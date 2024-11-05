2259258fa032713731286ec8032ebecc9949adeb
yonghuikim <kyhui1115@gmail.com>
2024-11-05 15:39:05 +0900

    design: css 수정

--- src/app/survey/reports/_components/_reportSearch/subjectTableContainer.tsx
+++ src/app/survey/reports/_components/_reportSearch/subjectTableContainer.tsx
@@ -67,7 +67,7 @@ export default function SubjectTableContainer() {
           dataSource={dataSource}
           rowKey="id"
           bordered={true}
-          scroll={{ y: 560 }}
+          scroll={{ y: 573 }}
           onRow={(data) => {
             return {
               onClick: () => {
