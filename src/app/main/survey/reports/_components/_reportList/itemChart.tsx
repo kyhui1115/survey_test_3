b4df233b7376334b22bbed27ae252e1237a7be05
yonghuikim <kyhui1115@gmail.com>
2024-11-08 14:15:25 +0900

    remove: 필요없는 기능 삭제

--- src/app/main/survey/reports/_components/_reportList/itemChart.tsx
+++ src/app/main/survey/reports/_components/_reportList/itemChart.tsx
@@ -1,5 +1,4 @@
 import useGetDashboard from "@/_api/_query/useGetDashboard";
-import useDelayChart from "@/_hooks/useDelayChart";
 import { useEffect, useState } from "react";
 import { Bar, BarChart, Rectangle, Tooltip, XAxis, YAxis } from "recharts";
 
@@ -12,7 +11,6 @@ export default function ItemChart({ subjectId, reportId }: props) {
   const [chartHeight, setChartHeight] = useState(0);
 
   const { data: dashboard } = useGetDashboard(subjectId, reportId);
-  const showChart = useDelayChart(dashboard);
 
   const data = dashboard?.[0]?.data;
 
@@ -24,37 +22,35 @@ export default function ItemChart({ subjectId, reportId }: props) {
 
   return (
     <>
-      {showChart && (
-        <div
-          className="w-1/4 justify-center items-center flex border-r border-border-gray relative"
-          style={{ height: chartHeight, minHeight: 250 }}
-        >
-          <div className="absolute right-4">
-            <BarChart
-              width={360}
-              height={chartHeight}
-              data={data}
-              layout="vertical"
-            >
-              <XAxis type="number" />
-              <YAxis
-                type="category"
-                dataKey="title"
-                width={140}
-                tick={{ fontSize: 10, fontWeight: 600 }}
-              />
-              <Tooltip />
-              <Bar
-                dataKey="score"
-                name="점수"
-                fill="rgb(96 165 250)"
-                activeBar={<Rectangle fill="rgb(37 99 235)" />}
-                animationEasing="ease-in-out"
-              />
-            </BarChart>
-          </div>
+      <div
+        className="w-1/4 justify-center items-center flex border-r border-border-gray relative"
+        style={{ height: chartHeight, minHeight: 250 }}
+      >
+        <div className="absolute right-4">
+          <BarChart
+            width={360}
+            height={chartHeight}
+            data={data}
+            layout="vertical"
+          >
+            <XAxis type="number" />
+            <YAxis
+              type="category"
+              dataKey="title"
+              width={140}
+              tick={{ fontSize: 10, fontWeight: 600 }}
+            />
+            <Tooltip />
+            <Bar
+              dataKey="score"
+              name="점수"
+              fill="rgb(96 165 250)"
+              activeBar={<Rectangle fill="rgb(37 99 235)" />}
+              animationEasing="ease-in-out"
+            />
+          </BarChart>
         </div>
-      )}
+      </div>
     </>
   );
 }
